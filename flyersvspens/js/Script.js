07f035f176192fd452339aef5a759401a4d29c19
Mikael Gerhardsson <mikael.gerhardsson@gmail.com>
2016-02-07 17:53:41 +0100

    Added testdata

--- flyersvspens/js/Script.js
+++ flyersvspens/js/Script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     console.log("Document ready.");
+    
+    $.support.cors = true;
+    $.ajax({
+        type : 'GET',
+        dataType : 'jsonp',
+        url: '../testdata.jsonp',
+        success : function(data) {
+            console.log(data); 
+        } 
+    });
+
 });
